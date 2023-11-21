245faaf61b84f80835f7df68b100a1dffcac60ac
dimi1985 <xpilot88@hotmail.com>
2023-11-21 19:10:44 +0200

    vercel update

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const cors = require('cors'); // Import cors
 
 const app = express();
 
 // Middleware for parsing JSON
 app.use(express.json());
+
+
+// Middleware for CORS
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://root:Svr9QXnKOYroT9qB@rest-recipe-api-db.cfupl.mongodb.net/?retryWrites=true&w=majority', {
